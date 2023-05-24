a314417f4df6c870d562b6f7f80279beafbbca3d
Wagner <wprodriguesbr@gmail.com>
2023-05-24 17:05:19 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+/**
+ * Maior valor obtido: 718
+ * 
+ * - Brisa leve: 10 ~ 150
+ * 
+ * - Brisa moderada: 151 ~ 300
+ * 
+ * - Vento forte: 301 ~450
+ * 
+ * - Ventania forte: 451 ~ 650
+ * 
+ * - Tempestade: 651 ~ 800
+ */
+basic.forever(function () {
+	
+})
